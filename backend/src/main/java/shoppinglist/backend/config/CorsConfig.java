9d6d52f8c54fe91eaf05ef5088ef2c190ab32b40
LeonMois <leon_m95@web.de>
2026-02-15 13:57:46 +0100

    add deployment script

--- backend/src/main/java/shoppinglist/backend/config/CorsConfig.java
+++ backend/src/main/java/shoppinglist/backend/config/CorsConfig.java
@@ -10,7 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "https://leonmoyseevich.com",
+                        "https://www.leonmoyseevich.com"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
